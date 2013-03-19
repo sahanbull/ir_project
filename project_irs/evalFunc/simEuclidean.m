d35398678be3db3f09960b91ce9cc3152a16abff
Sahanbull <sahanbull@gmail.com>
2013-03-19 13:56:16 +0000

    added inverse to keep the score scales standard
    Signed-off-by: Sahanbull <sahanbull@gmail.com>

--- project_irs/evalFunc/simEuclidean.m
+++ project_irs/evalFunc/simEuclidean.m
@@ -1,4 +1,5 @@
 %% scoreEuc: this function computes score using euclidian distance between two vectors
 function [score] = simEuclidean(query,comparer)
-	score = norm(query-comparer);
+	% closest has to have higher score.. hence, inverse
+	score = 1/(norm(query-comparer));
 end
