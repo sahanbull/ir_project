1abdb6f1269601339ce9f7c393a7115ccdf81075
Sahanbull <sahanbull@gmail.com>
2013-03-19 22:14:55 +0000

    averagePrecision computing function implemented
    Signed-off-by: Sahanbull <sahanbull@gmail.com>

--- project_irs/retrivalEval/averagePrecision.m
+++ project_irs/retrivalEval/averagePrecision.m
@@ -1,4 +1,21 @@
 %% averagePrecision: this function calculates the average precsion for the query
 function [AP] = averagePrecision(queryCl,resultsCl)
-	AP = 0;
+	resultsCl
+	queryCl
+
+	% pick the locations of the relevant returns
+	relResults = find(resultsCl==queryCl)
+
+	AP = 0.0; % initiate Average precision
+	correct = 0; % to store # of correct returns
+	
+	% foreach correct location
+	for(i=1:length(relResults))
+		correct = correct + 1;
+		% average precision = former average precision + # correct / # total till now
+		% and # total till now = location of the correct one
+		AP = AP + correct./relResults(i,1);
+	end
+	% to devide by # relavnt documents in the result
+	AP = AP./correct;	
 end
