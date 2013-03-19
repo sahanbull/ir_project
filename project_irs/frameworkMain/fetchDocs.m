e71770b5c4aaca556364c441e5eede700dc8b475
Sahanbull <sahanbull@gmail.com>
2013-03-19 19:28:49 +0000

    added line to fetch the classes of the relevant documents
    Signed-off-by: Sahanbull <sahanbull@gmail.com>

--- project_irs/frameworkMain/fetchDocs.m
+++ project_irs/frameworkMain/fetchDocs.m
@@ -1,8 +1,11 @@
 %% fetchDocs: this function fetches the documents in a matrix 
-function [docs] = fetchDocs(docList)
+function [docs,classes] = fetchDocs(docList)
 	% load the file with corpus:: changed later
 	load('testDummyData.mat');
 
 	% pick the ones that are relevant
 	docs = comparers(docList,:);
+
+	% picks the relevant classes of the docs
+	classes = classCorpus(docList,1);
 end
