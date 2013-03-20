6bb60bef6aa54a04846316bb761aaf5edf97f8e3
Sahanbull <sahanbull@gmail.com>
2013-03-20 14:50:18 +0000

    vectorized average precision calculation
    Signed-off-by: Sahanbull <sahanbull@gmail.com>

--- project_irs/retrivalEval/averagePrecision.m
+++ project_irs/retrivalEval/averagePrecision.m
@@ -5,17 +5,15 @@ function [AP] = averagePrecision(queryCl,resultsCl)
 
 	% pick the locations of the relevant returns
 	relResults = find(resultsCl==queryCl);
+	% 1 over total number of docs at each relevant return 
+	% devide that value by the total relavent documents in the result
+	relResults2 = 1./(relResults.*length(relResults));
 
-	AP = 0.0; % initiate Average precision
-	correct = 0; % to store # of correct returns
-	
-	% foreach correct location
-	for(i=1:length(relResults))
-		correct = correct + 1;
-		% average precision = former average precision + # correct / # total till now
-		% and # total till now = location of the correct one
-		AP = AP + correct./relResults(i,1);
-	end
-	% to devide by # relavnt documents in the result
-	AP = AP./correct;	
+	% number of rel document per each correct location
+	corrects = (1:length(relResults));
+
+	% average precision- vector multiplication 
+	% (sumproduct of: # relevant x 1/# all docs at that point / # total relevant)
+	AP = (corrects*relResults2);
+
 end
