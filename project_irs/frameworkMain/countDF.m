c2a5a669a8e6457d43069152c8c721ac26651414
Sahanbull <sahanbull@gmail.com>
2013-03-21 02:04:59 +0000

    changed variable
    Signed-off-by: Sahanbull <sahanbull@gmail.com>

--- project_irs/frameworkMain/countDF.m
+++ project_irs/frameworkMain/countDF.m
@@ -1,22 +1,22 @@
 %% countDF: this function returns the list of document frequency for each word in a feature set
 function [idfs] = countDF()
-	load('testDummyData.mat');
+	load('processedHOG.mat');
 
-	l = length(IDF);
+	l = length(InvDocIndex);
 	idfs = zeros(l,1);
 
 	% % start with first feature doc list 
-	docList = IDF{1,1}; % assign to union set
-	idfs(1,1) = length(IDF{1,1}); % count the df
+	docList = InvDocIndex{1,1}; % assign to union set
+	idfs(1,1) = length(InvDocIndex{1,1}); % count the df
 	
 	% foreach following feature 
 	for(i=2:l)
 		% eleminates duplicate entries and takes union
-		docList = union(docList,IDF{1,i});
-		idfs(i,1) = length(IDF{1,i});
+		docList = union(docList,InvDocIndex{i,1});
+		idfs(i,1) = length(InvDocIndex{i,1});
 	end
 
-	N = length(docList); % number of total docs in the collection
+	N = length(docList) % number of total docs in the collection
 
 	idfs = log10(N./idfs); % compute IDF scores
 end
