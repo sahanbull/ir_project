8f0de8958ac759d69c850e4dc582379826d87d7a
Sahanbull <sahanbull@gmail.com>
2013-03-21 02:04:27 +0000

    changed file name
    Signed-off-by: Sahanbull <sahanbull@gmail.com>

--- project_irs/evalFunc/getIdfs.m
+++ project_irs/evalFunc/getIdfs.m
@@ -1,6 +1,6 @@
 %% getIdfs: this function picks the IDF values required for a perticular query
 function [Idfs] = getIdfs(features)
-	load('testDummyData.mat');
+	load('processedHOG.mat');
 
 	% fetch the required IDFs
 	Idfs = idfs(features,1);
